c2b617f2691c498cd572797628e1569fa97e7c91
jsuqu <jsuquia26@gmail.com>
2024-12-18 18:16:30 +0100

    Primer commit: subida de archivos PHP

---
+++ empezando.php
@@ -0,0 +1,15 @@
+<h1>
+    <?= 50 + "1";?>
+</h1>
+
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+    body {
+        display: grid;
+        place-content: center;
+    }
+
+</style>
